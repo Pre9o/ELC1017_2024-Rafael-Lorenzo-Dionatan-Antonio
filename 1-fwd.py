beeb2587317e52b1c4aa63b2b80e4eae91e038cf
Rafael Carneiro Pregardier <102990182+Pre9o@users.noreply.github.com>
2024-11-07 16:31:04 -0300

    test fwd

--- 1-fwd.py
+++ 1-fwd.py
@@ -3,14 +3,22 @@
 from scapy.all import *
 
 def example(pkt):
-	pkt.show()
-	if pkt.sniffed_on == 'r-eth1' and pkt[IP].dst == '10.2.2.1':
-		pkt[Ether].dst = None
-		sendp(pkt, iface='r-eth2')
-	elif pkt.sniffed_on == 'r-eth2' and pkt[IP].dst == '10.1.1.1':
-		pkt[Ether].dst = None
-		sendp(pkt, iface='r-eth1')
-	else:
-		return
-		
-sniff(iface=["r-eth1","r-eth2"], filter='ip',  prn=example)
+    pkt.show()  # Exibe o pacote capturado para verificação
+
+    # Encaminha pacotes do h1 para h2 através das interfaces corretas
+    if pkt.sniffed_on == 'r1-eth1' and pkt[IP].dst == '10.2.2.1':
+        pkt_fwd = pkt.copy()  # Cria uma cópia do pacote
+        del pkt_fwd[Ether]  # Remove o cabeçalho Ethernet para reescrita automática
+        sendp(pkt_fwd, iface='r1-eth2')
+    
+    # Encaminha pacotes do h2 para h1 através das interfaces corretas
+    elif pkt.sniffed_on == 'r1-eth2' and pkt[IP].dst == '10.1.1.1':
+        pkt_fwd = pkt.copy()  # Cria uma cópia do pacote
+        del pkt_fwd[Ether]
+        sendp(pkt_fwd, iface='r1-eth1')
+    
+    else:
+        return  # Ignora pacotes que não se destinam ao encaminhamento
+
+# Inicia a captura de pacotes nas interfaces r1-eth1 e r1-eth2
+sniff(iface=["r1-eth1", "r1-eth2"], filter='ip', prn=example)

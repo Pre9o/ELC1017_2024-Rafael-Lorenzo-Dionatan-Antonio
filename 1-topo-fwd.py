e0162c50264eddfe38dc3e7c8187ddd4357b721c
Rafael Carneiro Pregardier <102990182+Pre9o@users.noreply.github.com>
2024-11-06 21:46:31 -0300

    go back topology

--- 1-topo-fwd.py
+++ 1-topo-fwd.py
@@ -4,39 +4,27 @@ from mininet.node import Node
 from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 
-class AdvancedTopo(Topo):
-    "Two routers with two hosts"
+class BasicTopo(Topo):
+    "A LinuxRouter connecting two hosts"
 
     def build(self, **_opts):
-        # Create routers
-        r1 = self.addHost('r1', ip=None)
-        r2 = self.addHost('r2', ip=None)
+        router = self.addHost('r', ip=None)
+        host1 = self.addHost('h1', ip=None, defaultRoute='via 10.1.1.254')
+        host2 = self.addHost('h2', ip=None, defaultRoute='via 10.2.2.254') 
 
-        # Create hosts
-        h1 = self.addHost('h1', ip=None, defaultRoute='via 10.1.1.254')
-        h2 = self.addHost('h2', ip=None, defaultRoute='via 10.2.2.254')
-        
-        # Host to Router 1
-        self.addLink(h1, r1, intfName1='h1-eth0', params1={'ip': '10.1.1.1/24'},
-                     intfName2='r1-eth1', params2={'ip': '10.1.1.254/24'})
-        
-        # Host to Router 2
-        self.addLink(h2, r2, intfName1='h2-eth0', params1={'ip': '10.2.2.1/24'},
-                     intfName2='r2-eth1', params2={'ip': '10.2.2.254/24'})
-        
-        # Router to Router link
-        self.addLink(r1, r2, intfName1='r1-eth2', params1={'ip': '10.3.3.1/24'},
-                     intfName2='r2-eth2', params2={'ip': '10.3.3.2/24'})
+        self.addLink(host1, router, 
+                     intfName1='h1-eth0', params1={'ip':'10.1.1.1/24'},
+                     intfName2='r-eth1', params2={'ip':'10.1.1.254/24'})
+        self.addLink(host2, router, 
+                     intfName1='h2-eth0', params1={'ip':'10.2.2.1/24'},
+                     intfName2='r-eth2', params2={'ip':'10.2.2.254/24'})
 
 def run():
-    "Advanced topology with two routers and route exchange testing"
-    net = Mininet(topo=AdvancedTopo(), controller=None)
-    
-    # Disable offload features for proper packet handling in Scapy
+    "Basic example"
+    net = Mininet(topo=BasicTopo(), controller=None)
     for _, v in net.nameToNode.items():
         for itf in v.intfList():
-            v.cmd('ethtool -K ' + itf.name + ' tx off rx off')
-    
+            v.cmd('ethtool -K ' + itf.name + ' tx off rx off') 
     net.start()
     CLI(net)
     net.stop()

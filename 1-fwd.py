cf50b24ec41ebb0b9f31f392e0bed31aa6daaf53
Rafael Carneiro Pregardier <102990182+Pre9o@users.noreply.github.com>
2024-11-07 19:41:23 -0300

    att fwd

--- 1-fwd.py
+++ 1-fwd.py
@@ -7,15 +7,13 @@ def example(pkt):
 
     # Encaminha pacotes do h1 para h2 através das interfaces corretas
     if pkt.sniffed_on == 'r1-eth1' and pkt[IP].dst == '10.2.2.1':
-        pkt_fwd = pkt.copy()  # Cria uma cópia do pacote
-        del pkt_fwd[Ether]  # Remove o cabeçalho Ethernet para reescrita automática
-        sendp(pkt_fwd, iface='r1-eth2')
+        pkt[Ether].dst = None
+        sendp(pkt, iface='r1-eth2')
     
     # Encaminha pacotes do h2 para h1 através das interfaces corretas
     elif pkt.sniffed_on == 'r1-eth2' and pkt[IP].dst == '10.1.1.1':
-        pkt_fwd = pkt.copy()  # Cria uma cópia do pacote
-        del pkt_fwd[Ether]
-        sendp(pkt_fwd, iface='r1-eth1')
+        pkt[Ether].dst = None
+        sendp(pkt, iface='r1-eth1')
     
     else:
         return  # Ignora pacotes que não se destinam ao encaminhamento

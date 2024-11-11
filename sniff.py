9de81dbb7c3c4b97d4517984b548174cede13a03
Rafael Carneiro Pregardier <102990182+Pre9o@users.noreply.github.com>
2024-11-11 20:03:05 -0300

    att 2

--- sniff.py
+++ sniff.py
@@ -38,4 +38,4 @@ def example(pkt):
         process_route_packet(pkt)  # Processa a tabela... TO DO
 
 # Inicia a captura na interface especificada
-sniff(iface='r-eth1', filter=f"ip proto {ROUTE_PROTO_ID}", prn=example)
+sniff(iface='r5-eth1', filter=f"ip proto {ROUTE_PROTO_ID} or ip", prn=example)

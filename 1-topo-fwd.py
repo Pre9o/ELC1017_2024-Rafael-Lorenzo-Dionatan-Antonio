6e25f63fe826eda9a95d3e30cad1f949629ab17e
Rafael Carneiro Pregardier <102990182+Pre9o@users.noreply.github.com>
2024-11-18 19:28:37 -0300

    sla 2

--- 1-topo-fwd.py
+++ 1-topo-fwd.py
@@ -37,7 +37,7 @@ class AdvancedTopo(Topo):
         self.addLink(h3, r3, intfName1='h3-eth1', params1={'ip': '10.3.3.1/24'}, intfName2='r3-eth4', params2={'ip': '10.3.3.254/24'})
         self.addLink(r3, h4, intfName1='r3-eth5', params1={'ip': '10.4.4.254/24'}, intfName2='h4-eth0', params2={'ip': '10.4.4.1/24'})
         self.addLink(r2, h4, intfName1='r2-eth4', params1={'ip': '10.14.14.254/24'}, intfName2='h4-eth1', params2={'ip': '10.14.14.1/24'})  # Corrigido
-        self.addLink(h4, r4, intfName1='h4-eth2', params1={'ip': '10.4.4.1/24'}, intfName2='r4-eth3', params2={'ip': '10.4.4.254/24'})
+        self.addLink(h4, r4, intfName1='h4-eth2', params1={'ip': '10.14.14.1/24'}, intfName2='r4-eth3', params2={'ip': '10.14.14.254/24'})  # Corrigido
         self.addLink(h5, r2, intfName1='h5-eth0', params1={'ip': '10.5.5.1/24'}, intfName2='r2-eth5', params2={'ip': '10.5.5.254/24'})
         self.addLink(h6, r4, intfName1='h6-eth0', params1={'ip': '10.6.6.1/24'}, intfName2='r4-eth5', params2={'ip': '10.6.6.254/24'})
         self.addLink(r3, h7, intfName1='r3-eth6', params1={'ip': '10.7.7.254/24'}, intfName2='h7-eth0', params2={'ip': '10.7.7.1/24'})
